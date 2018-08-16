a88906f4443f63a517ef700f6216a33dfa30ee70
Mats Egnell <mattegol.egnell@gmail.com>
2018-08-16 13:01:15 +0200

    Add comment in HomeController

--- Testy/Controllers/HomeController.cs
+++ Testy/Controllers/HomeController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Testy.Models;
 
 namespace Testy.Controllers
 {
     public class HomeController : Controller
     {
+        // Comment added
         public IActionResult Index()
         {
             return View();
