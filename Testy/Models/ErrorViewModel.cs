fbfe378923537323e1d2b03a16a3ce25aacfe733
Mats Egnell <mattegol.egnell@gmail.com>
2018-08-16 11:41:10 +0200

    Add project files.

---
+++ Testy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Testy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
